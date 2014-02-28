5458dc0500dcf57886e8e12c1672a77c04ba9642
dchouzer <david.p.chou@gmail.com>
2014-02-28 18:17:17 -0500

    Browser example for viewing purposes, executive controller methods

--- src/controller/ExecutiveController.java
+++ src/controller/ExecutiveController.java
@@ -1,5 +1,19 @@
 package controller;
+import model.*;
 
 public class ExecutiveController {
 
+	private Parser myParser;
+	
+	public ExecutiveController() {
+		myParser = new Parser();
+		
+	}
+	
+	public void execute(String text) throws Exception {
+		myParser.parseProgram(text);
+	}
+	
+	
+	
 }
