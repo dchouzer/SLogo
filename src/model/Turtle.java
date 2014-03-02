0199189cd1eb06f9c75d32cb653541834de6f688
dchouzer <david.p.chou@gmail.com>
2014-03-01 20:19:34 -0500

    added Turtle methods - removed duplicate controller

--- src/model/Turtle.java
+++ src/model/Turtle.java
@@ -18,7 +18,16 @@ public class Turtle extends JGObject{
 		myAngle = 90.0;
 		isVisible = true;
 	}
+	
+	/*
+	 * Allows the model to place the turtle at a particular position
+	 */
+	public void setXY(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 
+	//Refactor the methods so that the movement is placed inside of the model
 	public void moveForward(double magnitude) {
 		x += magnitude * (Math.cos(myAngle)*180);
 		y += magnitude * (Math.sin(myAngle)*180);
@@ -31,7 +40,7 @@ public class Turtle extends JGObject{
 	
 	public void rotateLeft(double angleToRotate) {
 		myAngle += angleToRotate;
-		
+	
 	}
 
 	public void rotateRight(double angleToRotate) {
