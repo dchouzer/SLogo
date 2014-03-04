f382e5d347428ef3130cef014d7929571d92f6c9
dchouzer <david.p.chou@gmail.com>
2014-03-04 14:57:34 -0500

    evaluator method added

--- src/model/Evaluator.java
+++ src/model/Evaluator.java
@@ -15,6 +15,8 @@ public class Evaluator {
 	
 	public void execute(List<Command> list) {
 		for (Command c : list) {
+			//Store this value and send it to the view
+			//Display an operation to show up
 			c.execute();
 		}
 	}
